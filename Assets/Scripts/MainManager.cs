053b258d4926df2b6b88cf39ea8033fad551c3de
0blivi0n <aubry.varen@gmail.com>
2023-01-18 18:48:13 +0100

    modified MainManager

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -121,11 +121,11 @@ public class MainManager : MonoBehaviour
         }
     }
 
-    public void SaveGameRank(string bestPlaterName, int bestPlayerScore)
+    public void SaveGameRank(string bestPlayerName, int bestPlayerScore)
     {
         SaveData data = new SaveData();
 
-        data.TheBestPlayer = bestPlaterName;
+        data.TheBestPlayer = bestPlayerName;
         data.HighestScore = bestPlayerScore;
 
         string json = JsonUtility.ToJson(data);
